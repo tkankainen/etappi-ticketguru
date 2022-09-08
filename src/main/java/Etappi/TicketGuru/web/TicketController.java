9de48b00b8d4627cd61dae875d798c13abe1fdcf
AnnePe <anne.pelin@koodata.fi>
2022-09-08 15:03:36 +0300

    Spring Boot alustus

---
+++ src/main/java/Etappi/TicketGuru/web/TicketController.java
@@ -0,0 +1,21 @@
+package Etappi.TicketGuru.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class TicketController {
+	@RequestMapping(value = "/hello")
+	@ResponseBody
+	
+	/*public String login(){
+		return "/hello";
+	}*/
+	
+	public String Hello(){
+		return "TicketGuru aloitus Team Etappi : Elia, Taina, Pertti, Anne 8.9.2022 ";//selaimessa http://localhost:8080/hello
+	}
+
+}
