00606777ec5dafb922f457201833f6b9b564bd8b
Pertzamus <korhonen.pertti@hotmail.com>
2022-09-20 16:35:36 +0300

    User -> Kayttaja muunnos

--- TicketGuru/src/main/java/Etappi/TicketGuru/domain/MyyntitapahtumaRepository.java
+++ TicketGuru/src/main/java/Etappi/TicketGuru/domain/MyyntitapahtumaRepository.java
@@ -3,7 +3,7 @@ package Etappi.TicketGuru.domain;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MyyntitapahtumaRepository extends CrudRepository<User, Long> {
+public interface MyyntitapahtumaRepository extends CrudRepository<Kayttaja, Long> {
 
 //	List<User> findByName(String etunimi);
 
