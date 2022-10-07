608e1609b14c272315776cd73374771d85247028
AnnePe <anne.pelin@koodata.fi>
2022-10-07 10:42:59 +0300

    lisätty taphtumalipputyyppi get set ja validator

--- TicketGuru/src/main/java/Etappi/TicketGuru/domain/Tapahtumalipputyyppi.java
+++ TicketGuru/src/main/java/Etappi/TicketGuru/domain/Tapahtumalipputyyppi.java
@@ -24,21 +24,21 @@ public class Tapahtumalipputyyppi {
 	
 	private long hinta;
 	
-	@ManyToOne
+	@ManyToOne(optional = false)
 	@JoinColumn(name="tapahtumaid")
 	private Tapahtuma tapahtuma;  //monta lipputyyppiä/tapahtuma
 	
-	@ManyToOne
+	@ManyToOne(optional = false)
 	@JoinColumn(name="tyyppiid")
 	private Lipputyyppi lipputyyppi; //viiteavain lipputyyppi
-
-	public Tapahtumalipputyyppi(){}
 	
 	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL, mappedBy="tapahtumalipputyyppi")
 	private List <Lippu> liput;  //monta lippua tapahtumalipputyypissä
+
+	public Tapahtumalipputyyppi(){}
 	
-	
+			
 	public Tapahtumalipputyyppi(long hinta, Tapahtuma tapahtuma, Lipputyyppi lipputyyppi) {
 		super();
 		this.hinta = hinta;
@@ -47,10 +47,10 @@ public class Tapahtumalipputyyppi {
 	}
 
 	
-	public Tapahtumalipputyyppi(long hinta, Tapahtuma tapahtuma) {
+	/*public Tapahtumalipputyyppi(long hinta, Tapahtuma tapahtuma) {
 		this.hinta=hinta;
 		this.tapahtuma = tapahtuma;
-	}	
+	}	*/
 	
 	public long getLipputyyppiid() {
 		return lipputyyppiid;
@@ -67,6 +67,24 @@ public class Tapahtumalipputyyppi {
 	public void setHinta(long hinta) {
 		this.hinta = hinta;
 	}
+	
+	
+	public Tapahtuma getTapahtuma() {
+		return tapahtuma;
+	}
+
+	public void setTapahtuma(Tapahtuma tapahtuma) {
+		this.tapahtuma = tapahtuma;
+	}
+	
+	public Lipputyyppi getLipputyyppi() {
+		return lipputyyppi;
+	}
+
+	public void setLipputyyppi(Lipputyyppi lipputyyppi) {
+		this.lipputyyppi = lipputyyppi;
+	}
+	
 	public List<Lippu> getLiput(){
 		return liput;
 	}
