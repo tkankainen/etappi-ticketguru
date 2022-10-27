24da3cdc847e3ecc75fe08082c81bd42765a891d
AnnePe <anne.pelin@koodata.fi>
2022-10-27 19:33:59 +0300

    Findbylippukoodi lippurepository

--- TicketGuru/src/main/java/Etappi/TicketGuru/domain/LippuRepository.java
+++ TicketGuru/src/main/java/Etappi/TicketGuru/domain/LippuRepository.java
@@ -1,13 +1,15 @@
 package Etappi.TicketGuru.domain;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "lippus", path = "liput")
 public interface LippuRepository extends CrudRepository<Lippu, Long> {
 	
-	List<Lippu> findByLippukoodi(String lippukoodi);
-
+	//List<Lippu> findByLippukoodi(String lippukoodi);
+	List<Lippu> findByLippukoodi(@Param("koodi") UUID lippukoodi);
 }
