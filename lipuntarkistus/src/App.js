7e9c741ec9b3f909eb0974b08391f42f6acb6428
Taina <kankainen.taina@gmail.com>
2022-11-14 21:32:32 +0200

    navigaatio clienttiin: react-router, material-ui

--- lipuntarkistus/src/App.js
+++ lipuntarkistus/src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Lipputarkistus from "./components/Lipputarkistus";
+import Home from "./components/Home";
 
 function App() {
   return (
-       <div>
-        <Lipputarkistus/>
-       </div>
-         );
+    <div className="App">
+      <BrowserRouter>
+        <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Ticket Guru
+              </Typography>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link color="white" to="/">Home</Link>{' '} 
+              </Typography>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link to="/lipuntarkistus">Lipuntarkistus</Link>{' '} 
+              </Typography>
+            </Toolbar>
+        </AppBar>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/lipuntarkistus" element={<Lipputarkistus />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
