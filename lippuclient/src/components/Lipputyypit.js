a9a6ed2648410b5598754c6df29551a0af717ef7
Taina <kankainen.taina@gmail.com>
2022-11-18 19:42:48 +0200

    lipputyyppien lisäys

--- lippuclient/src/components/Lipputyypit.js
+++ lippuclient/src/components/Lipputyypit.js
@@ -1,16 +1,18 @@
 import { useEffect, useState, useRef } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import { useLocation } from 'react-router-dom'
+import LisaaLipputyyppi from './LisaaLipputyyppi';
 
 function Lipputyypit () {
 
     const [lipputyypit, setLipputyypit] = useState([]);
     const location = useLocation()
     const { lippuurl } = location.state
-    console.log(lippuurl)
+    const { tapahtumaurl } = location.state
 
     const fetchData = () => {
         console.log(lippuurl)
+        console.log(tapahtumaurl)
         fetch(lippuurl)
         .then(response => response.json())
         .then(data => setLipputyypit(data._embedded.tapahtumalipputyyppis))
@@ -21,6 +23,18 @@ function Lipputyypit () {
         fetchData();
     }, []);
 
+    const tallennaLipputyyppi = (lipputyyppi) => {
+        fetch("https://etappi-ticketguru.herokuapp.com/api/tapahtumalipputyypit/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(lipputyyppi)
+        })
+        .then(res => fetchData())
+        .catch(error => console.error(error))
+    }
+
     const columns = [
         { headerName: "Nimi", field: "nimi", sortable: true, filter: true, width: 160, resizable: true },
         { headerName: "Hinta", field: "hinta", sortable: true, filter: true, width: 160, resizable: true },
@@ -30,6 +44,10 @@ function Lipputyypit () {
 
     return (
         <div className="ag-theme-material" style={{height: 700, margin: 'auto'}}>
+        <LisaaLipputyyppi 
+            tallennaLipputyyppi={tallennaLipputyyppi}
+            tapahtumaurl={tapahtumaurl}
+        />
         <AgGridReact
             ref={gridRef}
             onGridReady={ params => gridRef.current= params.api }
