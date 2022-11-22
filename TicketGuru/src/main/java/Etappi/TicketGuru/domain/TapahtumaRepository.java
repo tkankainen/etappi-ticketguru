2e467666ee97a4243c297b9d53bd695e100e7d1b
AnnePe <anne.pelin@koodata.fi>
2022-11-22 16:14:35 +0200

    tapahtumarepository search

--- TicketGuru/src/main/java/Etappi/TicketGuru/domain/TapahtumaRepository.java
+++ TicketGuru/src/main/java/Etappi/TicketGuru/domain/TapahtumaRepository.java
@@ -3,10 +3,22 @@ package Etappi.TicketGuru.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
 @RepositoryRestResource(collectionResourceRel = "tapahtumas", path = "tapahtumat")
 public interface TapahtumaRepository extends CrudRepository<Tapahtuma, Long>{
-	List<Tapahtuma> findByNimi(String nimi);
+	//hae tapahtuma nimellä
+		@RestResource(path = "nimi", rel = "nimi")
+		List<Tapahtuma> findByNimi(@Param("nimi")String nimi);
+		
+		//hae kaupungilla
+		@RestResource(path = "kaupunki", rel = "kaupunki")
+		List<Tapahtuma> findByKaupunki(@Param("kaupunki")String kaupunki);
+		
+		//hae nimen osalla
+		@RestResource(path = "nimiosa", rel = "nimiosa")
+		List<Tapahtuma> findByNimiContaining(@Param("nimi")String nimi);
 }
