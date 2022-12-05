b69f012b77b00099c8cc5102d37a7b9c69fd7a96
Taina <kankainen.taina@gmail.com>
2022-12-05 20:05:21 +0200

    aloitussivu

--- lippuclient/src/components/Logout.js
+++ lippuclient/src/components/Logout.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 function Logout () {
 
+    const navigate = useNavigate();
+    const token = sessionStorage.getItem("jwt");
+    const [loggedOut, setLoggedOut] = useState(false)
+
+    useEffect(() => {
+        if(!token) {
+          navigate('/login');
+        }
+    }, [loggedOut]);
+
     const logout = () => {
         sessionStorage.removeItem("jwt");
+        setLoggedOut(true)
     }
     
     return (
